76108cc43eb8f598eb0e06e0a62a3b2e548baef7
Ron Gatenio <ron.gatenio@gmail.com>
2024-12-29 01:20:45 +0200

    added flush

--- pslab.h
+++ pslab.h
@@ -7,19 +7,34 @@
 #define PSLAB_H
 
 #include <libpmem.h>
+#include <emmintrin.h>
 
 #define	PSLAB_POLICY_DRAM 0
 #define	PSLAB_POLICY_PMEM 1
 #define	PSLAB_POLICY_BALANCED 2
 
+inline void _persist(char* data, size_t len)
+{
+    #define kCacheLineSize (64)
+
+    volatile char *ptr = (char*)((unsigned long)data & (~(kCacheLineSize-1)));
+    _mm_sfence();
+    for (; ptr < data+len; ptr+=kCacheLineSize) {
+        _mm_clflush((const void*)ptr);
+    }
+    _mm_sfence();
+}
+
+#define PERSIST(func, addr, size) do { func((addr), (size)); _persist((addr), (size)); } while(false)
+
 #define pmem_member_persist(p, m) \
-    pmem_persist(&(p)->m, sizeof ((p)->m))
+    PERSIST(pmem_persist, &(p)->m, sizeof ((p)->m))
 #define pmem_member_flush(p, m) \
-    pmem_flush(&(p)->m, sizeof ((p)->m))
+    PERSIST(pmem_flush, &(p)->m, sizeof ((p)->m))
 #define pmem_flush_from(p, t, m) \
-    pmem_flush(&(p)->m, sizeof (t) - offsetof(t, m));
-#define pslab_item_data_persist(it) pmem_persist((it)->data, ITEM_dtotal(it)
-#define pslab_item_data_flush(it) pmem_flush((it)->data, ITEM_dtotal(it))
+    PERSIST(pmem_flush, &(p)->m, sizeof (t) - offsetof(t, m))
+#define pslab_item_data_persist(it) PERSIST(pmem_persist, (it)->data, ITEM_dtotal(it))
+#define pslab_item_data_flush(it) PERSIST(pmem_flush, (it)->data, ITEM_dtotal(it))
 
 int pslab_create(char *pool_name, uint32_t pool_size, uint32_t slab_size,
     uint32_t *slabclass_sizes, int slabclass_num);
